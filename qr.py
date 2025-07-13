677d04f75e356d0c9e307a280eb7864e5f31db80
cbes502034 <cbes502034@gmail.com>
2025-07-13 12:03:49 +0800

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,5 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,Query,Form,Request
+from starlette.middleware.sessions import SessionMiddleware
+from typing import Annotated
+from fastapi.responses import JSONResponse,PlainTextResponse,HTMLResponse,FileResponse,RedirectResponse 
 from fastapi.staticfiles import StaticFiles
+from MySql.mysql import SQL
+from Image import IMG
+import pyotp
+KEY = "KEY"
 qr = FastAPI()
+sql = SQL(USER = "root",
+          PASSWORD = "wrTejwGDvnEFmGAkDkyHiENMBlAorNxb",
+          HOST = "shuttle.proxy.rlwy.net",
+          PORT = 11075,
+          DATABASE = "QRcode")
 
+def TotpAuthenticatorObject(secret,CODE=False):
+    obj = pyotp.TOTP(secret)
+    if CODE:
+        return obj.now()
+    return obj
+    
+qr.add_middleware(SessionMiddleware,secret_key=KEY)
+@qr.post("/login")
+async def Login(request:Request):
+    response = await request.json()
+    email = response["email"]
+    account = response["account"]
+    password = response["password"]
+    check = sql.execute(instruction = """SELECT *FROM login
+                                         WHERE email=%s AND account=%s AND password=%s""",
+                        SET = (email,account,password),
+                        SELECT = True)
+    if check:
+        
+        request.session["SECRET"] = pyotp.random_base32()
+        
+        Totp_object = TotpAuthenticatorObject(secret = request.session["SECRET"])
+        
+        uri = Totp_object.provisioning_uri(
+            name=email,
+            issuer_name="Google"
+        )
+        img_data_src = IMG.src(URI = uri)
+        
+        return JSONResponse({"status":True,
+                             "qrcode":img_data_src,
+                             "information":"qrcode已儲存，請掃描後輸入驗證碼",})
+    else:
+        return JSONResponse({"status":False})
+    
+@qr.post("/login/totp")
+async def LoginTotp(request:Request):
+    response = await request.json()
+    totp_input = response["totp"]
+    Totp_code = TotpAuthenticatorObject(secret = request.session["SECRET"],CODE=True)
+    if totp_input == Totp_code:
+        return JSONResponse({"status":True})
+    else:
+        return JSONResponse({"status":False})
 qr.mount("/",StaticFiles(directory="qrcode",html=True))
